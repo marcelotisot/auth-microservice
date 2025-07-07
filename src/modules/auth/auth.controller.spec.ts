b5748ba8534b35e9393b87cc0d15eccea0c8cb9c
Juan Marcelo Tisot <marcelotisot1990@gmail.com>
2025-07-07 16:17:53 -0300

    test(auth) fix dependencias de providers

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +10,11 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UsersService,
+        JwtService,
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
